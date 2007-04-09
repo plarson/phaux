bd24138e294b837a3aa811c3fe07fda84a8378a6
WHarford <WHarford@d3e93b25-b32d-0410-8836-6ddc07c251ce>
2007-04-09 22:24:33 +0000

    Renamed A bunch of stuff

---
+++ trunk/Classes/Phaux-test/phaux-test.php
@@ -0,0 +1,8 @@
+<?php
+
+include("WHFormTest.php");
+include("WHCounter.php");
+include("WHNavigationTest.php");
+include("WHDialogTest.php");
+include("WHMultiCounter.php");
+include("WHRegisteredCounter.php");
