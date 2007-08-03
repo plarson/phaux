df191ae27bfe32654e71e85b193a2ce89657bf40
WHarford <WHarford@d3e93b25-b32d-0410-8836-6ddc07c251ce>
2007-08-03 18:19:59 +0000

    Path Decoration

---
+++ trunk/Classes/Phaux-base/WHPathDecoration.php
@@ -0,0 +1,30 @@
+<?
+
+class WHPathDecoration extends WHWindowDecoration{
+
+	
+	public function closeWindow(){
+		$this->decoratedComponent->thisOrDialog()->answer(FALSE);
+		return $this;
+	}
+
+	public function renderCloseButtonOn($html){
+		if($this->decoratedComponent->thisOrDialog() === $this->decoratedComponent){
+			return $html->bold($this->title) . $html->br();
+		}else{
+			return $html->anchor()->
+					callback($this,'closeWindow')->
+					with($this->title).
+					$html->text(' > ');
+		}
+		
+	}
+
+	public function renderDecorationOn($html,$parentHTML){
+
+		return $html->span()->class('path-decoration')->with(
+					$this->renderCloseButtonOn($html)
+				).
+				$this->renderDecoratedComponentOn($html,$parentHTML);
+	}
+}
