54d635271ea0bb2757f585b9eb426075ba629fe0
WHarford <WHarford@d3e93b25-b32d-0410-8836-6ddc07c251ce>
2007-09-27 17:19:44 +0000

    remove the inclde_onces

--- trunk/Classes/REServe-Phaux/reserve-phaux.php
+++ trunk/Classes/REServe-Phaux/reserve-phaux.php
@@ -1,8 +1,8 @@
 <?php
-include_once("WHREServeSession.php");
-include_once("WHREServeDisplay.php");
-include_once("WHREServeTest.php");
-include_once("WHREServeRootModel.php");
-include_once("WHREServeContactModel.php");
-include_once("WHREServeModelEdit.php");
-include_once("WHREServeList.php");
+include("WHREServeSession.php");
+include("WHREServeDisplay.php");
+include("WHREServeTest.php");
+include("WHREServeRootModel.php");
+include("WHREServeContactModel.php");
+include("WHREServeModelEdit.php");
+include("WHREServeList.php");
