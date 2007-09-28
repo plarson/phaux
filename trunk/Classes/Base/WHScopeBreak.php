3f68305890879657c699caa2cdf8bce3c8d6ed38
WHarford <WHarford@d3e93b25-b32d-0410-8836-6ddc07c251ce>
2007-09-28 18:27:21 +0000

    Missed a couple classes

---
+++ trunk/Classes/Base/WHScopeBreak.php
@@ -0,0 +1,15 @@
+<?php
+
+class WHScopeBreak extends WHProxyObjectLogCall{
+	protected $__loggingEnabled = FALSE;
+	public function __get($nm){
+
+		return $this->__object->getIvarNamed($nm);
+	}
+	
+	public function __set($nm,$value){
+		//	die(var_dump($this));
+		return $this->__object->setIvarNamed($nm,$value);
+	}
+
+}
---
+++ trunk/Classes/Phaux-base/WHWorkspace.php
@@ -0,0 +1,60 @@
+<?php
+
+class WHWorkspace extends WHComponent {
+	protected $code = '';
+	protected $results = '';
+	protected $context;
+	
+	public function evalCode($phpCode){
+		$this->code = $phpCode;
+		ob_start();
+		evalWithThis($this->context,$phpCode);
+		$this->results = ob_get_flush();
+		return $this;
+	}
+	
+	public function setContext($anObject){
+		$this->context = Object::construct('WHScopeBreak')->__setObject($anObject);
+		return $this;
+	}
+	
+	public function context(){
+		return $this->context;
+	}
+	
+	public function renderResultsOn($html){
+		if($this->results != ''){
+			return $html->div()->class('whinspector-results')->with($this->results);
+		}
+		return '';
+	}
+	
+	public function renderContentOn($html){
+		return $html->div()->class('whinspector')->with(
+				$this->renderResultsOn($html).
+				$html->form()->with(
+					$html->textArea()->with($this->code)->
+					callback($this,'evalCode').$html->br().
+					$html->submitButton()->value('Evaluate Code!')
+				));
+	}
+	
+	public function style(){
+		return '
+			.whinspector textarea{
+				width:600px;
+				height:200px;
+			} 
+		';
+	}
+	
+}
+
+/*
+**used so we can redifine this
+*/
+function evalWithThis($__this,$phpCode){
+
+	$phpCode = str_replace('$this','$__this',$phpCode);
+	return eval($phpCode);
+}
