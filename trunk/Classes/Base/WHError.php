173b3434dd7e63560f9ebdf00e7ef512ba8514af
WHarford <WHarford@d3e93b25-b32d-0410-8836-6ddc07c251ce>
2007-10-03 04:54:41 +0000

    Woops. Dont know what I was thinking there.

---
+++ trunk/Classes/Base/WHError.php
@@ -0,0 +1,55 @@
+<?php
+
+class WHError extends Object {
+	protected $number;
+	protected $string;
+	protected $file;
+	protected $line;
+	
+	public function number(){
+		return $this->number;
+	}
+	
+	public function setNumber($anInteger){
+		$this->number = $anInteger;
+		return $this;
+	}
+	
+	public function string(){
+		return $this->string;
+	} 
+	public function setString($aString){
+		$this->string = $aString;
+		return $this;
+	}
+	
+	public function file(){
+		return $this->file;
+	}
+	public function setFile($aString){
+		$this->file = $aString;
+		return $this;
+	}
+	
+	public function line(){
+		return $this->line;
+	}
+	public function setLine($aNumber){
+		$this->line = $aNumber;
+		return $this;
+	}
+	
+	public function isUserError(){
+		return $this->number == E_USER_ERROR || 
+				$this->number == E_USER_WARNING ||
+				$this->number == E_USER_NOTICE;
+	}
+	public function isPhpError(){
+		return !$this->isUserError();
+	}
+	
+	public function __toString(){
+		return $this->string. ' in '.$this->file.' on '.$this->line;
+	}
+	
+}
