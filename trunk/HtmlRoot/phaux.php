47dd66cc071bf66356d75aa401f41554d420f0f5
WHarford <WHarford@d3e93b25-b32d-0410-8836-6ddc07c251ce>
2007-10-08 19:03:55 +0000

    reserve text, new resource locater, and some clean up of phaux.php

--- trunk/HtmlRoot/phaux.php
+++ trunk/HtmlRoot/phaux.php
@@ -20,67 +20,46 @@ if(get_magic_quotes_gpc()){
 }
 
 include("../Classes/Base/base.php");
+include("../Classes/Phaux-base/phaux-base.php");
 $errorHandler = Object::construct("WHErrorHandler")->start();
-$base_configuration = parse_ini_file("../Configuration/base.ini",TRUE);
 $app_configurations = array();
 
-if(!isset($_REQUEST['app'])){
-	$path_portions = explode('/', $_SERVER['PATH_INFO']);
-	$app = $path_portions[1];
-	$_REQUEST['app'] = $app;
-}else{
-	$app = $_REQUEST['app'];
-}
-//var_dump($_SERVER);
+$app = WHConfiguration::currentApplicationName();
 
 /* FIXME
 ** Currently Phaux parses the INI file on every request 
 ** This is excessive. We should only parse the ini file
 ** at the start of a session and use the configuration 
 ** from the last session if present
+**
+** The problem is that we can't restore the session untill
+** the includes have been processes and the includes
+** are stored in the session and in the INI files
 */
-if(eregi('^[A-Z_0-9_.]*$', $app) && file_exists('../Configuration/'.$app.'.ini')){
-	$app_configurations[$app] = $base_configuration;
-	$new_conf = parse_ini_file('../Configuration/'.$app.'.ini',TRUE);
-	
-	foreach($new_conf as $section => $values){
-		if(isset($base_configuration[$section]) && is_array($base_configuration[$section])){
-			$new_conf[$section] = array_merge($base_configuration[$section],$values);
-		}
-	}
-	foreach($base_configuration as $section =>$values){
-		if(!isset($new_conf[$section])){
-			$new_conf[$section] = $values;
-		}
-	}
-	
-	$app_configurations[$app] = $new_conf;
-}
 
+$app_configurations[$app] = WHConfiguration::parseConfigurationFileForApp($app);
 
 if($app_configurations[$app] == NULL){
-	header("HTTP/1.0 404 Not Found");
-	echo("<h1>No such application $app </h1>");
 	$errorHandler->end();
-	exit;
+	WHConfiguration::render404();
 }else{
-	
-	
+
+	/*
+	**We must include before we construct the configuration
+	*/
 	foreach($app_configurations[$app]['includes'] as $var => $value){
-		include("$value");
-	}	
+		include($value);
+	}
 	
-	ini_set("session.use_cookies",$app_configurations[$app]['general']['use_cookie']);
-	ini_set("session.name","SID");
-	 
 	$configuration_class = $app_configurations[$app]['general']['configuration_class'];
 	$configuration = Object::construct($configuration_class);
 	$configuration->setApplicationName($app)->
 										setConfigValues($app_configurations[$app]);
 	
-	
-	$session_class = $app_configurations[$app]['general']['session_class'];
-	
+	ini_set("session.use_cookies",$configuration->useCookie());
+	ini_set("session.name","SID");
+			
+	$session_class = $configuration->sessionClass();
 	$session = call_user_func(array(
 						Object::construct($session_class),
 						"startSessionOnAppWithConfiguration"),$app,$configuration);
@@ -138,7 +117,7 @@ if(isset($_REQUEST["_i"]) && is_array($_REQUEST["_i"])){
 }
 
 if(!isset($_SESSION[$app]['mainComponent'])){
-	$main_class = $app_configurations[$app]['general']['main_class'];
+	$main_class = $configuration->mainClass();
 	$_SESSION[$app]['mainComponent'] = Object::construct($main_class);
 	if($configuration->debugMode()){
 		$_SESSION[$app]['mainComponent']->addDecoration(
@@ -158,7 +137,7 @@ if(isset($_REQUEST['_lu'])){
 
 if($REDIRECT){
 	
-	if($app_configurations[$app]['general']['redirect_after_callback'] == 1){
+	if($configuration->redirectAfterCallback() == 1){
 		$_SESSION[$app]['session']->save();
 		$urlExtra = $htmlRoot->getExtraUrl();
 		header("Location: ".$_SESSION[$app]['session']->url().$urlExtra);
@@ -174,11 +153,11 @@ if($REDIRECT){
 	}
 }
 
-foreach($app_configurations[$app]['styles'] as $var => $value){
+foreach($configuration->styles() as $var => $value){
 	$htmlRoot->needsStyle($value);
 }
 
-foreach($app_configurations[$app]['scripts'] as $var => $value){
+foreach($configuration->scripts() as $var => $value){
 	$htmlRoot->needsScript($value);
 }
 
@@ -192,7 +171,7 @@ if($configuration->debugMode()){
 	}
 }
 	
-$html = WHHtmlCanvas::construct($app_configurations[$app]['general']['render_class']);
+$html = WHHtmlCanvas::construct($configuration->renderClass());
 
 if(!isset($_REQUEST['_lu']) || $_REQUEST['_lu'] == ""){
 	
---
+++ trunk/HtmlRoot/resource.php
@@ -0,0 +1,50 @@
+<?php
+/*
+** This file will serve up resoureces that are outside
+** your sourece directory. 
+** Specify the resource directories in your ini file 
+** under the section resources
+*/
+include("../Classes/Base/base.php");
+include("../Classes/Phaux-base/phaux-base.php");
+$app = WHConfiguration::currentApplicationName();
+if(!$app){
+	WHConfiguration::render404();
+}
+$config = WHConfiguration::parseConfigurationFileForApp($app);
+$resourceDirs = $config['resources'];
+$path_portions = explode('/', $_SERVER['PATH_INFO']);
+$resource = $path_portions[2];
+
+if(!eregi('^[A-Z_0-9_.]*$', $resource)){
+	WHConfiguration::render404();
+	exit;
+}
+
+$typesForExt = array('css'=>'text/css',
+					'html'=>'text/html',
+					'jpg'=>'image/jpeg',
+					'gif'=>'image/gif',
+					'png'=>'image/png');
+
+foreach($resourceDirs as $dir){
+	if(is_file($dir.'/'.$resource)){
+		$offset = 3600 * 24;	
+		header('Expires: ' . gmdate("D, d M Y H:i:s", time() + $offset) . ' GMT');
+		header('Cache-control: public, must-revalidate');
+	  	header('Content-Length: '.filesize($dir.'/'.$resource));
+		$gmt_mtime = gmdate('D, d M Y H:i:s', filemtime($dir.'/'.$resource) ) . ' GMT';
+		header('Last-Modified: '.$gmt_mtime);
+		$eparts = explode('.',$resource);
+		$ext = $eparts[count($eparts)-1];
+		$mime = $typesForExt[$ext];
+		if(!$mime){
+			$mime = 'application/octet-stream';
+		}
+		header('Content-Type: '.$mime);
+		readfile($dir.'/'.$resource);
+	}
+	
+}
+
+WHConfiguration::render404();
