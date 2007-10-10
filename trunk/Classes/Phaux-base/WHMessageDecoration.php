84097b87eac75c89d4178532a9f8e3cad2219a65
WHarford <WHarford@d3e93b25-b32d-0410-8836-6ddc07c251ce>
2007-10-10 13:22:16 +0000

    Woops added MessageDecoration

---
+++ trunk/Classes/Phaux-base/WHMessageDecoration.php
@@ -0,0 +1,20 @@
+<?php
+
+class WHMessageDecoration extends WHDecoration {
+	protected $message;
+	
+	public function message(){
+		return $this->message;
+	}
+	
+	public function setMessage($aString){
+		$this->message = $aString;
+		return $this;
+	}
+	
+	public function renderDecorationOn($html,$parentHtml){
+		return $html->headingLevel(1)->class('message')->with($this->message).
+					$this->renderDecoratedComponentOn($html,$parentHtml);
+				
+	}
+}
